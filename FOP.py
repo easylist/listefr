d1152db79a52534cc9c4380b6f65459b3b71e835
Crits <none@none>
2013-07-13 13:28:24 +0200

    M: commented out running of addChecksum.pl in FOP.py

--- FOP.py
+++ FOP.py
@@ -140,8 +140,8 @@ def main (location):
                     # Ignore errors resulting from deleting files, as they likely indicate that the file has already been deleted
                     pass
 	# Add checksum
-    varliste = "liste_fr.txt"
-    pipe = subprocess.call(["perl", "addChecksum.pl", varliste])
+    # varliste = "liste_fr.txt"
+    # pipe = subprocess.call(["perl", "addChecksum.pl", varliste])
     # If in a repository, offer to commit any changes
     if repository:
         commit(repository, basecommand, originaldifference)
